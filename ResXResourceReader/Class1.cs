3160ae8697b6d9a606b5e84bc225216db1bc4cfa
ststeiger <quandary82@hailmail.net>
2018-08-23 17:38:46 +0200

    Projektdateien hinzufügen.

---
+++ ResXResourceReader/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ResXResourceReader
+{
+    public class Class1
+    {
+    }
+}
---
+++ TestResxReader/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TestResxReader
+{
+
+
+    class Program
+    {
+
+        public static void ReadRessourceFile()
+        {
+            //Requires Assembly System.Windows.Forms '
+            string filename = @"C:\Program Files\paint.net\resx\PaintDotNet.Strings.3.resx";
+            System.Resources.ResXResourceReader rsxr = new System.Resources.ResXResourceReader(filename);
+
+            // Iterate through the resources and display the contents to the console. '    
+            foreach (System.Collections.DictionaryEntry d in rsxr)
+            {
+                System.Console.WriteLine(d.Key.ToString() + ":\t" + d.Value.ToString());
+            }
+
+            //Close the reader. '
+            rsxr.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            ReadRessourceFile();
+            System.Console.WriteLine("Hello World!");
+        }
+
+
+    }
+
+
+}
