da10e294bfaf6f3e390d3f2f663988c6cdca1e8f
Me <me@no.spam>
2018-08-23 18:13:47 +0200

    Fix resx

--- TestResxReader/Program.cs
+++ TestResxReader/Program.cs
@@ -10,12 +10,20 @@ namespace TestResxReader
         {
             //Requires Assembly System.Windows.Forms '
             string filename = @"C:\Program Files\paint.net\resx\PaintDotNet.Strings.3.resx";
+            filename = "/root/github/EntityFrameworkCore/src/EFCore.Sqlite.Core/Properties/SqliteStrings.resx";
+            filename = "/root/github/ExpressProfiler/ExpressProfiler/MainForm.resx";
             System.Resources.ResXResourceReader rsxr = new System.Resources.ResXResourceReader(filename);
 
             // Iterate through the resources and display the contents to the console. '    
             foreach (System.Collections.DictionaryEntry d in rsxr)
             {
-                System.Console.WriteLine(d.Key.ToString() + ":\t" + d.Value.ToString());
+                if(d.Key != null)
+                    System.Console.Write(d.Key.ToString() + ":\t");
+                
+                if(d.Value != null) 
+                    System.Console.Write(d.Value.ToString());
+                
+                System.Console.WriteLine();
             }
 
             //Close the reader. '
